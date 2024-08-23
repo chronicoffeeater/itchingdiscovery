f0aad023f91e346f24eba07f90d69e3f323a699b
Glitch (itchingdiscovery) <none>
2024-08-23 20:24:26 +0000

    so it shows up on every birthday

--- public/all.js
+++ public/all.js
@@ -1,16 +1,23 @@
 /* This code runs on *all* pages */
 
 document.addEventListener("load", function () {
-  document.querySelector("nav").insertAdjacentHTML(
-    "afterEnd",
-    `
-  <div class="section">
-    <h1>It's @breakfast_for_dinner's birthday!!!</h1>
-      Wish her a happy birthday :)
-      <br>
-      <a href="https://scratch.mit.edu/users/breakfast_for_dinner/#comments">https://scratch.mit.edu/users/breakfast_for_dinner/</a>
-  </div>`
-  );
-  
-  document.title += ' | Itching Discovery';
+  let today = new Date();
+  if (
+    today.getMonth() === 7 &&
+    today.getDate() === 23
+  ) {
+    document.querySelector("nav").insertAdjacentHTML(
+      "afterEnd",
+      `
+      <div class="section">
+        <h1>It's @breakfast_for_dinner's birthday!!!</h1>
+          Wish her a happy birthday :)
+          <br>
+          <a href="https://scratch.mit.edu/users/breakfast_for_dinner/#comments">https://scratch.mit.edu/users/breakfast_for_dinner/</a>
+      </div>
+  `
+    );
+  }
+
+  document.title += " | Itching Discovery";
 });
